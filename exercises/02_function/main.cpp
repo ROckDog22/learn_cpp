ed86957cbb8035b38894331e093a606da67e5efb
rockdog22 <wangfengsheng1999@gamil.com>
2024-07-22 18:00:21 +0000

    init

---
+++ exercises/02_function/main.cpp
@@ -0,0 +1,16 @@
+#include "../exercise.h"
+
+// TODO: 在这里声明函数
+int add(int, int);
+
+int main(int argc, char **argv) {
+    ASSERT(add(123, 456) == 123 + 456, "add(123, 456) should be 123 + 456");
+
+    auto x = 1, y = 2;
+    std::cout << x << " + " << y << " = " << add(x, y) << std::endl;
+    return 0;
+}
+
+int add(int a, int b) {
+    return a+b;
+}
---
+++ xmake.lua
@@ -0,0 +1,21 @@
+add_rules("mode.debug", "mode.release")
+set_encodings("utf-8")
+set_warnings("all")
+set_languages("cxx17")
+
+target("test")
+    set_kind("static")
+    add_defines(string.format("__XMAKE__=\"%s\"", os.scriptdir():gsub("\\", "/")))
+    add_files("learn/test.cpp")
+
+target("learn")
+    set_kind("binary")
+    add_deps("test")
+    add_files("learn/learn.cpp")
+
+target("summary")
+    add_cxflags("-pthread")
+    add_ldflags("-pthread")
+    set_kind("binary")
+    add_deps("test")
+    add_files("learn/summary.cpp")
